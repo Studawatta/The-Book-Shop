b3dddd703eee095400aad8195360b0deab2d8547
Studawatta <st.udawatte@gmail.com>
2023-04-13 23:37:30 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.DB_PW,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+});
+// db.connect(() => {
+//   if (err) {
+//     console.log('Error connecting to DB');
+//   } else {
+//     console.log('Connected to Database');
+//   }
+// });
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json('hello this is the backend');
+});
+app.get('/books', (req, res) => {
+  const q = 'SELECT * FROM books';
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post('/books', (req, res) => {
+  const q = 'INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)';
+  const values = [req.body.title, req.body.desc, req.body.cover];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json('Book has been created successfully');
+  });
+});
+app.listen(8800, () => {
+  console.log('Connected to backend!');
+});
